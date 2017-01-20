27fd39bfdec438b2f948ec4a58c674fed4cce01d
Labeeb Mattra <mmlabeeb@gmail.com>
2017-01-20 16:49:36 +0530

    proxies paramater passing for all get and post

--- frappeclient/frappeclient.py
+++ frappeclient/frappeclient.py
@@ -2,243 +2,257 @@ import requests
 import json
 from StringIO import StringIO
 
+
 class AuthError(Exception):
-	pass
+    pass
+
 
 class FrappeException(Exception):
-	pass
+    pass
+
 
 class NotUploadableException(FrappeException):
-	def __init__(self, doctype):
-		self.message = "The doctype `{1}` is not uploadable, so you can't download the template".format(doctype)
+
+    def __init__(self, doctype):
+        self.message = "The doctype `{1}` is not uploadable, so you can't download the template".format(
+            doctype)
 
 CAN_DOWNLOAD = []
 
+
 class FrappeClient(object):
-	def __init__(self, url, username, password, proxies=None, pool=None):
-		self.session = requests.Session()
-		self.url = url
-		self.login(username, password)
-		self.proxies = proxies if proxies else {}
 
-		if pool:
-			requests.packages.urllib3.disable_warnings()
-			reqadapter = requests.adapters.HTTPAdapter(**pool)
-			self.session.mount("https://", reqadapter)
+    def __init__(self, url, username, password, proxies=None, pool=None):
+        self.session = requests.Session()
+        self.url = url
+        self.login(username, password)
+        self.proxies = proxies if proxies else {}
 
-	def __enter__(self):
-		return self
+        if pool:
+            requests.packages.urllib3.disable_warnings()
+            reqadapter = requests.adapters.HTTPAdapter(**pool)
+            self.session.mount("https://", reqadapter)
 
-	def __exit__(self, *args, **kwargs):
-		self.logout()
+    def __enter__(self):
+        return self
 
-	def login(self, username, password):
-		global CAN_DOWNLOAD
-		r = self.session.post(self.url, data={
-			'cmd': 'login',
-			'usr': username,
-			'pwd': password
-		})
+    def __exit__(self, *args, **kwargs):
+        self.logout()
 
-		if r.json().get('message') == "Logged In":
-			CAN_DOWNLOAD = []
-			return r.json()
-		else:
-			raise AuthError
+    def login(self, username, password):
+        global CAN_DOWNLOAD
+        r = self.session.post(self.url, data={
+            'cmd': 'login',
+            'usr': username,
+            'pwd': password
+        },
+            proxies=self.proxies)
 
-	def logout(self):
-		global CAN_DOWNLOAD
-		self.session.get(self.url, params={
-			'cmd': 'logout',
-		})
-		CAN_DOWNLOAD = []
+        if r.json().get('message') == "Logged In":
+            CAN_DOWNLOAD = []
+            return r.json()
+        else:
+            raise AuthError
 
-	def insert(self, doc):
-		res = self.session.post(self.url + "/api/resource/" + doc.get("doctype"),
-			data={"data":json.dumps(doc)})
-		return self.post_process(res)
+    def logout(self):
+        global CAN_DOWNLOAD
+        self.session.get(
+            self.url, params={
+                'cmd': 'logout',
+            },
+            proxies=self.proxies)
+        CAN_DOWNLOAD = []
 
-	def update(self, doc):
-		url = self.url + "/api/resource/" + doc.get("doctype") + "/" + doc.get("name")
-		res = self.session.put(url, data={"data":json.dumps(doc)})
-		return self.post_process(res)
+    def insert(self, doc):
+        res = self.session.post(self.url + "/api/resource/" + doc.get("doctype"),
+                                data={"data": json.dumps(doc)}, proxies=self.proxies)
+        return self.post_process(res)
 
-	def bulk_update(self, docs):
-		return self.post_request({
-			"cmd": "frappe.client.bulk_update",
-			"docs": json.dumps(docs)
-		})
+    def update(self, doc):
+        url = self.url + "/api/resource/" + \
+            doc.get("doctype") + "/" + doc.get("name")
+        res = self.session.put(url, data={"data": json.dumps(doc)})
+        return self.post_process(res)
 
-	def delete(self, doctype, name):
-		return self.post_request({
-			"cmd": "frappe.client.delete",
-			"doctype": doctype,
-			"name": name
-		})
+    def bulk_update(self, docs):
+        return self.post_request({
+            "cmd": "frappe.client.bulk_update",
+            "docs": json.dumps(docs)
+        })
 
-	def submit(self, doclist):
-		return self.post_request({
-			"cmd": "frappe.client.submit",
-			"doclist": json.dumps(doclist)
-		})
+    def delete(self, doctype, name):
+        return self.post_request({
+            "cmd": "frappe.client.delete",
+            "doctype": doctype,
+            "name": name
+        })
 
-	def get_value(self, doctype, fieldname=None, filters=None):
-		return self.get_request({
-			"cmd": "frappe.client.get_value",
-			"doctype": doctype,
-			"fieldname": fieldname or "name",
-			"filters": json.dumps(filters)
-		})
+    def submit(self, doclist):
+        return self.post_request({
+            "cmd": "frappe.client.submit",
+            "doclist": json.dumps(doclist)
+        })
 
-	def set_value(self, doctype, docname, fieldname, value):
-		return self.post_request({
-			"cmd": "frappe.client.set_value",
-			"doctype": doctype,
-			"name": docname,
-			"fieldname": fieldname,
-			"value": value
-		})
+    def get_value(self, doctype, fieldname=None, filters=None):
+        return self.get_request({
+            "cmd": "frappe.client.get_value",
+            "doctype": doctype,
+            "fieldname": fieldname or "name",
+            "filters": json.dumps(filters)
+        })
 
-	def cancel(self, doctype, name):
-		return self.post_request({
-			"cmd": "frappe.client.cancel",
-			"doctype": doctype,
-			"name": name
-		})
+    def set_value(self, doctype, docname, fieldname, value):
+        return self.post_request({
+            "cmd": "frappe.client.set_value",
+            "doctype": doctype,
+            "name": docname,
+            "fieldname": fieldname,
+            "value": value
+        })
 
-	def get_doc(self, doctype, name="", filters=None, fields=None):
-		params = {}
-		if filters:
-			params["filters"] = json.dumps(filters)
-                if fields:
-                        params["fields"] = json.dumps(fields)
+    def cancel(self, doctype, name):
+        return self.post_request({
+            "cmd": "frappe.client.cancel",
+            "doctype": doctype,
+            "name": name
+        })
 
-		res = self.session.get(self.url + "/api/resource/" + doctype + "/" + name,
-			params=params)
+    def get_doc(self, doctype, name="", filters=None, fields=None):
+        params = {}
+        if filters:
+            params["filters"] = json.dumps(filters)
+        if fields:
+            params["fields"] = json.dumps(fields)
 
-		return self.post_process(res)
+        res = self.session.get(self.url + "/api/resource/" + doctype + "/" + name,
+                               params=params, proxies=self.proxies)
 
-	def rename_doc(self, doctype, old_name, new_name):
-		params = {
-			"cmd": "frappe.client.rename_doc",
-			"doctype": doctype,
-			"old_name": old_name,
-			"new_name": new_name
-		}
-		return self.post_request(params)
+        return self.post_process(res)
 
-	def get_pdf(self, doctype, name, print_format="Standard", letterhead=True):
-		params = {
-			'doctype': doctype,
-			'name': name,
-			'format': print_format,
-			'no_letterhead': int(not bool(letterhead))
-		}
-		response = self.session.get(
-			self.url + "/api/method/frappe.templates.pages.print.download_pdf",
-			params=params, stream=True)
+    def rename_doc(self, doctype, old_name, new_name):
+        params = {
+            "cmd": "frappe.client.rename_doc",
+            "doctype": doctype,
+            "old_name": old_name,
+            "new_name": new_name
+        }
+        return self.post_request(params)
 
-		return self.post_process_file_stream(response)
+    def get_pdf(self, doctype, name, print_format="Standard", letterhead=True):
+        params = {
+            'doctype': doctype,
+            'name': name,
+            'format': print_format,
+            'no_letterhead': int(not bool(letterhead))
+        }
+        response = self.session.get(
+            self.url + "/api/method/frappe.templates.pages.print.download_pdf",
+            params=params, stream=True, proxies=self.proxies)
 
-	def get_html(self, doctype, name, print_format="Standard", letterhead=True):
-		params = {
-			'doctype': doctype,
-			'name': name,
-			'format': print_format,
-			'no_letterhead': int(not bool(letterhead))
-		}
-		response = self.session.get(
-			self.url + '/print', params=params, stream=True
-		)
-		return self.post_process_file_stream(response)
+        return self.post_process_file_stream(response)
 
-	def __load_downloadable_templates(self):
-		global CAN_DOWNLOAD
-		CAN_DOWNLOAD = self.get_api('frappe.core.page.data_import_tool.data_import_tool.get_doctypes')
+    def get_html(self, doctype, name, print_format="Standard", letterhead=True):
+        params = {
+            'doctype': doctype,
+            'name': name,
+            'format': print_format,
+            'no_letterhead': int(not bool(letterhead))
+        }
+        response = self.session.get(
+            self.url + '/print', params=params, stream=True, proxies=self.proxies
+        )
+        return self.post_process_file_stream(response)
 
-	def get_upload_template(self, doctype, with_data=False):
-		global CAN_DOWNLOAD
-		if not CAN_DOWNLOAD:
-			self.__load_downloadable_templates()
+    def __load_downloadable_templates(self):
+        global CAN_DOWNLOAD
+        CAN_DOWNLOAD = self.get_api(
+            'frappe.core.page.data_import_tool.data_import_tool.get_doctypes')
 
-		if doctype not in CAN_DOWNLOAD:
-			raise NotUploadableException(doctype)
+    def get_upload_template(self, doctype, with_data=False):
+        global CAN_DOWNLOAD
+        if not CAN_DOWNLOAD:
+            self.__load_downloadable_templates()
 
-		params = {
-			'doctype': doctype,
-			'parent_doctype': doctype,
-			'with_data': 'Yes' if with_data else 'No',
-			'all_doctypes': 'Yes'
-		}
+        if doctype not in CAN_DOWNLOAD:
+            raise NotUploadableException(doctype)
 
-		request = self.session.get(self.url +
-			'/api/method/frappe.core.page.data_import_tool.exporter.get_template',
-			params=params)
-		return self.post_process_file_stream(request)
+        params = {
+            'doctype': doctype,
+            'parent_doctype': doctype,
+            'with_data': 'Yes' if with_data else 'No',
+            'all_doctypes': 'Yes'
+        }
 
-	def get_api(self, method, params={}):
-		res = self.session.get(self.url + "/api/method/" + method + "/",
-			params=params)
-		return self.post_process(res)
+        request = self.session.get(self.url +
+                                   '/api/method/frappe.core.page.data_import_tool.exporter.get_template',
+                                   params=params, proxies=self.proxies)
+        return self.post_process_file_stream(request)
 
-	def post_api(self, method, params={}):
-		res = self.session.post(self.url + "/api/method/" + method + "/",
-			params=params)
-		return self.post_process(res)
+    def get_api(self, method, params={}):
+        res = self.session.get(self.url + "/api/method/" + method + "/",
+                               params=params, proxies=self.proxies)
+        return self.post_process(res)
 
-	def get_request(self, params):
-		res = self.session.get(self.url, params=self.preprocess(params), proxies=self.proxies)
-		res = self.post_process(res)
-		return res
+    def post_api(self, method, params={}):
+        res = self.session.post(self.url + "/api/method/" + method + "/",
+                                params=params, proxies=self.proxies)
+        return self.post_process(res)
 
-	def post_request(self, data):
-		res = self.session.post(self.url, data=self.preprocess(data), proxies=self.proxies)
-		res = self.post_process(res)
-		return res
+    def get_request(self, params):
+        res = self.session.get(self.url, params=self.preprocess(
+            params), proxies=self.proxies)
+        res = self.post_process(res)
+        return res
 
-	def preprocess(self, params):
-		"""convert dicts, lists to json"""
-		for key, value in params.iteritems():
-			if isinstance(value, (dict, list)):
-				params[key] = json.dumps(value)
+    def post_request(self, data):
+        res = self.session.post(
+            self.url, data=self.preprocess(data), proxies=self.proxies)
+        res = self.post_process(res)
+        return res
 
-		return params
+    def preprocess(self, params):
+        """convert dicts, lists to json"""
+        for key, value in params.iteritems():
+            if isinstance(value, (dict, list)):
+                params[key] = json.dumps(value)
 
-	def post_process(self, response):
-		try:
-			rjson = response.json()
-		except ValueError:
-			print response.text
-			raise
+        return params
 
-		if rjson and ("exc" in rjson) and rjson["exc"]:
-			raise FrappeException(rjson["exc"])
-		if 'message' in rjson:
-			return rjson['message']
-		elif 'data' in rjson:
-			return rjson['data']
-		else:
-			return None
+    def post_process(self, response):
+        try:
+            rjson = response.json()
+        except ValueError:
+            print response.text
+            raise
 
-	def post_process_file_stream(self, response):
-		if response.ok:
-			output = StringIO()
-			for block in response.iter_content(1024):
-				output.write(block)
-			return output
+        if rjson and ("exc" in rjson) and rjson["exc"]:
+            raise FrappeException(rjson["exc"])
+        if 'message' in rjson:
+            return rjson['message']
+        elif 'data' in rjson:
+            return rjson['data']
+        else:
+            return None
 
-		else:
-			try:
-				rjson = response.json()
-			except ValueError:
-				print response.text
-				raise
+    def post_process_file_stream(self, response):
+        if response.ok:
+            output = StringIO()
+            for block in response.iter_content(1024):
+                output.write(block)
+            return output
 
-			if rjson and ("exc" in rjson) and rjson["exc"]:
-				raise FrappeException(rjson["exc"])
-			if 'message' in rjson:
-				return rjson['message']
-			elif 'data' in rjson:
-				return rjson['data']
-			else:
-				return None
+        else:
+            try:
+                rjson = response.json()
+            except ValueError:
+                print response.text
+                raise
+
+            if rjson and ("exc" in rjson) and rjson["exc"]:
+                raise FrappeException(rjson["exc"])
+            if 'message' in rjson:
+                return rjson['message']
+            elif 'data' in rjson:
+                return rjson['data']
+            else:
+                return None
