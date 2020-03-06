971e7301303d61bb17a6c7531d705ada875170a8
Jason Hamilton <jwh@laydros.net>
2020-03-06 10:45:10 -0500

    fix print statement for python 3

--- frappeclient/frappeclient.py
+++ frappeclient/frappeclient.py
@@ -286,7 +286,7 @@ class FrappeClient(object):
 			try:
 				rjson = response.json()
 			except ValueError:
-				print response.text
+				print(response.text)
 				raise
 
 			if rjson and ("exc" in rjson) and rjson["exc"]:
